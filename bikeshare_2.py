76aff9f6e31a22419f192a66269e05f2958dc7bf
Eng.Naif_Moh <ka8ka-rmsb@hotmail.com>
2021-02-13 14:14:57 +0300

    Improve readability of load_data()

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -65,8 +65,6 @@ def load_data(city, month, day):
     df = pd.read_csv(CITY_DATA[city])
     
     df['Start Time'] = pd.to_datetime(df['Start Time'])
-    
-    
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
     
