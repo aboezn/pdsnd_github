5bdb6702fe06acae5377180436280426e9f293cf
Eng.Naif_Moh <ka8ka-rmsb@hotmail.com>
2021-02-13 13:51:41 +0300

    Add my info in def main()

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -189,7 +189,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
-            print('\nThank you, \nSee you later\n')
+            print('\nThank you, \nSee you later\n\nCode made by:\nNaif Moh \nYou can find me in Github:\ngithub.com/aboezn')
             break
 
 
